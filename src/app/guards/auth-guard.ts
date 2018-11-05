e9b635520c4561aee7aada2a38cf1b7624b82f1a
Rodrigo Jose Lopes <rjlopes@brq.com>
2018-11-05 10:52:15 -0200

    primeiro commit

---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,29 @@
+
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router
+
+  ) {  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso() {
+    //  this.router.navigate(['/login']);
+
+    return false;
+ }
+
+}
