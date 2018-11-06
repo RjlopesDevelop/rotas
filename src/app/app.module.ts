1d350bf9d800614949db46c5abad93d2aac66bc4
Rodrigo Jose Lopes <rjlopes@brq.com>
2018-11-06 15:23:02 -0200

    implementação de pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { ptbrLocale } from './shared/pt-br-locale';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
 import { AppRoutingModule } from './app-routing.module';
+import { SettingsService } from './shared/settings.service';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+
 // import { MaterializeModule } from 'angular2-materialize';
 
+registerLocaleData(localePt);
 
 
 @NgModule({
@@ -20,7 +28,18 @@ import { AppRoutingModule } from './app-routing.module';
     HttpModule,
    // MaterializeModule
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: ptbrLocale
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/pt-br-locale.ts
@@ -0,0 +1,2 @@
+
+export const ptbrLocale = settingsService => settingsService.getLocale();
